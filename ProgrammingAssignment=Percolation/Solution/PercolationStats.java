34dd990580c248cbfb0c79f4448b4a6d0792df57
Hexdrift <150795432+SARTHAK-RAUTELA@users.noreply.github.com>
2026-02-17 12:04:43 +0530

    stats code

---
+++ ProgrammingAssignment=Percolation/Solution/PercolationStats.java
@@ -0,0 +1,59 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+
+    private final double[] thresholds;
+    private final int trials;
+
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0)
+            throw new IllegalArgumentException("n and trials must be > 0");
+
+        this.trials = trials;
+        thresholds = new double[trials];
+
+        for (int t = 0; t < trials; t++) {
+            Percolation perc = new Percolation(n);
+
+            while (!perc.percolates()) {
+                int row = StdRandom.uniformInt(1, n + 1);
+                int col = StdRandom.uniformInt(1, n + 1);
+                perc.open(row, col);
+            }
+
+            thresholds[t] =
+                (double) perc.numberOfOpenSites() / (n * n);
+        }
+    }
+
+    public double mean() {
+        return StdStats.mean(thresholds);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(thresholds);
+    }
+
+    public double confidenceLo() {
+        return mean() - (1.96 * stddev() / Math.sqrt(trials));
+    }
+
+    public double confidenceHi() {
+        return mean() + (1.96 * stddev() / Math.sqrt(trials));
+    }
+
+    public static void main(String[] args) {
+
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+
+        PercolationStats stats = new PercolationStats(n, trials);
+
+        System.out.println("mean                    = " + stats.mean());
+        System.out.println("stddev                  = " + stats.stddev());
+        System.out.println("95% confidence interval = [" +
+                stats.confidenceLo() + ", " +
+                stats.confidenceHi() + "]");
+    }
+}
